25a9e53560f653d5fc3fd6554e08c207939ff6cb
setyaagung <setyaagung74@gmail.com>
2020-06-29 10:22:26 +0700

    memperbaiki tabel informasi

---
+++ database/migrations/2020_06_29_101221_create_informasi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInformasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informasi', function (Blueprint $table) {
+            $table->bigIncrements('id_informasi');
+            $table->bigInteger('id_user')->unsigned();
+            $table->string('judul');
+            $table->enum('kategori', ['Berita', 'Pengumuman']);
+            $table->text('isi');
+            $table->string('slug');
+            $table->string('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informasi');
+    }
+}
